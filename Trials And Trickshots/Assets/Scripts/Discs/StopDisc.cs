39c1fcca807ab3ac6b26b11bcf82c64eaa83affc
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2025-02-02 19:17:24 -0600

    Added the ability for the disc to bounce off of the ground

--- Trials And Trickshots/Assets/Scripts/Discs/StopDisc.cs
+++ Trials And Trickshots/Assets/Scripts/Discs/StopDisc.cs
@@ -10,6 +10,8 @@ using UnityEngine;
 
 public class StopDisc : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
+
     [Header("Shield variables")]
     [Tooltip("While active, the disc will bounce off the ground instead of stopping")]
     [SerializeField] private bool shielded;
@@ -17,11 +19,18 @@ public class StopDisc : MonoBehaviour
     [Tooltip("How many times the shield can hit the ground before it breaks")]
     [SerializeField] private int shieldHits;
 
+    [Tooltip("How many seconds it has before the shield can lose another hit")]
+    [SerializeField] private float shieldInvulnerabilityTime;
+
+    [Tooltip("How much the disc will bounce off the ground while shielded")]
+    [SerializeField] private float upwardsBounce;
+
     [Header("Box Cast variables")]
     [SerializeField] private LayerMask ground;
     [SerializeField] private Collider discBox;
 
     private bool grounded = false;
+    private bool canHitShield = true;
 
     /// <summary>
     /// Checks for when the disc hits the ground
@@ -49,13 +58,11 @@ public class StopDisc : MonoBehaviour
         //    }
         //}
 
-        if(GetComponent<Rigidbody>().velocity == Vector3.zero)
+        //Checks that the disc has hit the ground for the first time while not shielded.
+        //Also makes sure the shield is able to be hit again
+        if(HitGround() && !grounded && canHitShield)
         {
-            Debug.Log("Disc stopped");
-        }
-
-        if(HitGround() && !grounded)
-        {
+            //If no shield, enable friction to slow the disc down
             if(!shielded)
             {
                 grounded = true;
@@ -63,15 +70,41 @@ public class StopDisc : MonoBehaviour
             }
             else
             {
+                canHitShield = false;
+
+                StartCoroutine(EnableShieldDamage());
+
                 --shieldHits;
                 Debug.Log(shieldHits);
 
+                //Disables shield if it runs out of durability
                 if (shieldHits <= 0)
                 {
                     shielded = false;
                 }
+
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                rb.AddForce(Vector3.up * upwardsBounce, ForceMode.Impulse);
             }
         }
+    }
+
+    /// <summary>
+    /// Waits x amount of time before letting the shield take damage again.
+    /// Done so the shield doesn't take damage multiple times
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator EnableShieldDamage()
+    {
+        float i = shieldInvulnerabilityTime;
+        while (i >= 0)
+        {
+            yield return new WaitForSeconds(.1f);
+
+            i -= .1f;
+        }
+
+        canHitShield = true;
     }
 
     /// <summary>
