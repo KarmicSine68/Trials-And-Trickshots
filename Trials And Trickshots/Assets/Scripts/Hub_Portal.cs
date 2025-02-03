4fa5ea1750e9abe64be77f0cc6b45e0dd61f8889
BurnoutBeat <jakub.gorski5@gmail.com>
2025-02-02 20:20:08 -0600

    added extensive comments in code to explain how it works

--- Trials And Trickshots/Assets/Scripts/Hub_Portal.cs
+++ Trials And Trickshots/Assets/Scripts/Hub_Portal.cs
@@ -6,31 +6,26 @@ using UnityEngine.UIElements;
 
 public class Hub_Portal : MonoBehaviour
 {
-    [SerializeField] private GameObject _portal;
-    [SerializeField] private ParticleSystem _particleSystem;
-    [SerializeField] private string _level;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _portal;//reference to the physical portal
+    [SerializeField] private ParticleSystem _particleSystem;//when the disc/player 
+    [SerializeField] private string _level;//enter level name.
 
     private void OnTriggerEnter(Collider other)
     {
         if(other == null) return;
-        if(other.gameObject.tag == "disc" || other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "disc" || other.gameObject.tag == "Player") //checks to see if the player walks into the portal or the disc makes contact
         {
             //break particales
-                //BreakParticles();
+                //BreakParticles(); //disabled for now
             //activate teleport
-            SceneManager.LoadScene(_level);
+            SceneManager.LoadScene(_level);//sends them to level set in _level string in inspector.
         }
     }
 
     private void BreakParticles()
     {
-        GameObject firework = Instantiate(this.gameObject, this.transform.position, Quaternion.identity);
+        GameObject firework = Instantiate(this.gameObject, this.transform.position, Quaternion.identity);//creates particle 
 
-        firework.GetComponent<ParticleSystem>().Play();
+        firework.GetComponent<ParticleSystem>().Play();//plays particle for when the player/disc touches the portal 
     }
 }
