29e4ba812c639374ad9366d1c588d039bca4e499
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2025-02-06 16:47:12 -0600

    SCORE CARD
    owjddvb

---
+++ Trials And Trickshots/Assets/Scripts/ScoreCardButtonController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreCardButtonController : MonoBehaviour
+{
+    private ScoreManager sm;
+    private void Start()
+    {
+        sm = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+    public void ReturnToHubClicked()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ReplayLevelClicked()
+    {
+        sm.LogThrows(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
